0f137a9928f24d41aa5b28513577899c4a6828c4
priscillamc <priscillamc@users.noreply.github.com>
2018-05-06 14:36:30 -0400

    Adds initial customizations for style 1

--- functions.php
+++ functions.php
@@ -11,3 +11,33 @@ function ufchem_ufl_2011_enqueue_styles() {
  
 }
 add_action( 'wp_enqueue_scripts', 'ufchem_ufl_2011_enqueue_styles' );
+
+/**
+ * Add classes to the body tag based on theme options
+ * 
+ * @param array $classes
+ * @since 1.0
+ */
+function ufchem_ufl_2011_body_class( $classes ) {
+    $new_classes = array();
+    
+    // Add theme style class
+    $theme_style = 'ufchem-style-1';
+    $new_classes[] = $theme_style;
+    
+    return array_merge( $classes, $new_classes );
+}
+add_filter( 'body_class', 'ufchem_ufl_2011_body_class' );
+
+/**
+ * Override the institutional nav
+ * 
+ * @since 1.0
+ */
+if ( !function_exists('ufclas_responsive_top_nav') ){
+    function ufclas_responsive_top_nav(){
+        include get_stylesheet_directory() . '/library/php/uf-institutional-nav.php';
+    }  
+}
+
+
---
+++ library/php/uf-institutional-nav.php
@@ -0,0 +1,13 @@
+<nav id="institutional-nav" class="white palatino" role="navigation">
+  <ul id="top" class="container">
+    <li id="inst-home"><a href="http://www.ufl.edu/">University <em>of</em> Florida</a></li>
+    <li><a href="https://www.chem.ufl.edu/"><span>Department of Chemistry</span></a></li>
+    <li><a href="https://www.chem.ufl.edu/faculty-and-research/"><span>Research</span></a></li>
+      <li><a href="https://www.chem.ufl.edu/courses/"><span>Courses</span></a></li>
+    <li><a href="https://www.chem.ufl.edu/undergraduate/"><span>Undergraduate</span></a></li>
+    <li><a href="https://www.chem.ufl.edu/graduate/"><span>Graduate</span></a></li>
+    <li><a href="https://www.chem.ufl.edu/directory/"><span>People</span></a></li>
+      <li><a href="https://www.chem.ufl.edu/contact/"><span>Contact Us</span></a></li>
+  </ul> 
+</nav>
+ <!-- end #institutional-nav -->
